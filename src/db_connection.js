b15a55dc6abee20b2104a8caaa6d8a5242af9a2d
MorMauda <morchuka@gmail.com>
2020-08-24 20:35:00 -0400

    Update db_connection.js
    changing the db name

--- src/db_connection.js
+++ src/db_connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '12345678',
-    database: 'AfekaBook'
+    password: '',
+    database: 'Facebook_db'
 });
 
 connection.connect(function(err) {
